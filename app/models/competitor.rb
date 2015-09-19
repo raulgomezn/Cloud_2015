5f59c2ef39ede205cfef85b643ce1c490d700382
raulgomezn <raul.gomezn@hotmail.com>
2015-09-19 21:30:51 +0000

    Validaciones de los modelos

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -1,3 +1,12 @@
 class Competitor < ActiveRecord::Base
   belongs_to :competition
+  validates :first_name, length: { maximum: 50}, presence: true
+  validates :second_name, length: { maximum: 50}
+  validates :last_name, length: { maximum: 50}, presence: true
+  validates :second_last_name, length: { maximum: 50}, presence: true
+  validates :email, length: { maximum: 100}, presence: true
+  validates :message, length: { maximum: 255}
+  validates :status_video, length: { maximum: 50}
+  validates :url_video_original, length: { maximum: 255}, presence: true
+  validates :url_video_converted, length: { maximum: 255}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+    has_many :competitions
+    validates :firts_name, length: { maximum: 50}, presence: true
+    validates :second_name, length: { maximum: 50}
+    validates :last_name, length: { maximum: 50}, presence: true
+    validates :second_last_name, length: { maximum: 50}
+    validates :email, length: { maximum: 100, minimum: 10}, presence: true
+    validates :password, length: { maximum: 128, minimum: 6}, presence: true
 end
