c4eddbc869207471347fb23f5f4d24f86fb6a6ca
raulgomezn <raul.gomezn@hotmail.com>
2015-11-12 04:44:36 +0000

    Heroku 22 locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #before_action :set_locale
+  before_action :set_locale
   protect_from_forgery with: :exception
   include SessionsHelper
   
