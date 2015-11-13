157fcaceb13351f6a130e44aa9cafd212c4f829b
raulgomezn <raul.gomezn@hotmail.com>
2015-11-13 18:19:03 +0000

    memcached 2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Workspace::Application.config.session_store :cookie_store, key: '_workspace_session'
+#Workspace::Application.config.session_store :cookie_store, key: '_workspace_session'
+Workspace::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes
