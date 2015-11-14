2ca88e2eee472de9001bb35d4501d2df83e8bfa0
raulgomezn <raul.gomezn@hotmail.com>
2015-11-14 14:46:21 +0000

    correo 3

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,13 +2,13 @@ class UserMailer < ApplicationMailer
     # Envio de Email cuando crea cuenta
     def welcome_email(user)
         @user = user
-        @url  = 'http://smarttools.com/login'
+        @url  = 'http://unicloud2015.herokuapp.com/login'
         mail(to: @user.email, subject: t('welcome_for_email'))
     end
     # Envio de Email cuando se actualiza
     def update_email(user)
         @user = user
-        @url  = 'http://smarttools.com/login'
+        @url  = 'http://unicloud2015.herokuapp.com/login'
         mail(to: @user.email, subject: t('update_info'))
     end
 end
