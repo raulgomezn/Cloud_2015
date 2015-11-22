f4ea0aff2a0ddc6728e6878a3362f530711473ab
raulgomezn <raul.gomezn@hotmail.com>
2015-11-22 16:00:41 +0000

    escalar worker 4

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,5 +1,5 @@
 HireFire::Resource.configure do |config|
   config.dyno(:worker) do
-    HireFire::Macro::Bunny::Job.queue('test1', :amqp_url => ENV['CLOUDAMQP_URL'])
+    HireFire::Macro::Bunny.queue('test1', :amqp_url => ENV['CLOUDAMQP_URL'])
   end
 end
