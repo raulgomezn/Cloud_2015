c974aee595008be58476aa9cd69d334f5aca2a3e
raulgomezn <raul.gomezn@hotmail.com>
2015-11-29 04:50:02 +0000

    job 14

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,5 +1,16 @@
 HireFire::Resource.configure do |config|
   config.dyno(:worker) do
-    HireFire::Macro::Bunny.queue("test1", :connection => ENV['CLOUDAMQP_URL'])
+    #HireFire::Macro::Bunny.queue("test1", :connection => ENV['CLOUDAMQP_URL'])
+    puts 'Inicio Leer Cola hirefire'
+
+    b = Bunny.new ENV['CLOUDAMQP_URL']
+    b.start # start a communication session with the amqp server
+    
+    q = b.queue 'test1' # declare a queue
+    
+    puts q.message_count
+    b.stop # close the connection
+    b.close
+    puts "<---CLOSE cola hirefire"
   end
 end
