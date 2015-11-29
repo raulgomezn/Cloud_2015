f40ad39f8c88cfde2c84d14fdb2f05db1487edd7
raulgomezn <raul.gomezn@hotmail.com>
2015-11-29 05:28:10 +0000

    job 17

--- app/controllers/hirefire_controller.rb
+++ app/controllers/hirefire_controller.rb
@@ -1,27 +1,22 @@
 class HirefireController < ApplicationController
-  respond_to :json
-
-  def info
-    render json: JSON.generate([
-      {name: "worker", quantity: count_worker}
-    ])
-  end
-
-  private
-
-  def count_worker
-    puts 'Inicio Leer Cola HirefireController'
-
-    b = Bunny.new ENV['CLOUDAMQP_URL']
-    b.start # start a communication session with the amqp server
+    respond_to :json
     
-    q = b.queue 'test1' # declare a queue
-    
-    #puts q.message_count
-    b.stop # close the connection
-    b.close
-    puts "<---CLOSE cola HirefireController"
-    q.message_count
-  end
+    def info
+        puts 'Inicio Leer Cola HirefireController'
+
+        b = Bunny.new ENV['CLOUDAMQP_URL']
+        b.start # start a communication session with the amqp server
+        
+        q = b.queue 'test1' # declare a queue
+        
+        puts q.message_count
+        b.stop # close the connection
+        b.close
+        puts "<---CLOSE cola HirefireController"
+      
+        render json: JSON.generate([
+          {name: "worker", quantity: q.message_count}
+        ])
+    end
 
 end
